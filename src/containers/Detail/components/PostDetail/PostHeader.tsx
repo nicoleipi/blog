a34ab9934ec44e448ffd02f5a4b9b9938b6ed0bd
nicoleipi <pinicolei@gmail.com>
2023-06-23 23:11:34 -0700

    update category tag color

--- src/containers/Detail/components/PostDetail/PostHeader.tsx
+++ src/containers/Detail/components/PostDetail/PostHeader.tsx
@@ -1,5 +1,5 @@
 import { CONFIG } from "@/site.config"
-import { Cat_Tag } from "@components/Tag"
+import Cat_Tag from "@components/Tag"
 import { TPost } from "@/src/types"
 import { formatDate } from "@/src/libs/utils"
 import Image from "next/image"
@@ -44,7 +44,7 @@ const PostHeader: React.FC<Props> = ({ data }) => {
             {data.tags && (
               <div className="flex flex-nowrap max-w-full overflow-x-auto article-tags gap-2">
                 {data.tags.map((tag: string, idx:number) => (
-                  <Tag key={idx} category_id={idx}>{tag}</Tag>
+                  <Cat_Tag key={idx} category_id={idx}>{tag}</Cat_Tag>
                 ))}
               </div>
             )}
