530c30641d4cc4d6af18985e13914f83d7a17a72
nicoleipi <pinicolei@gmail.com>
2023-06-23 22:10:22 -0700

    update hovers

--- src/containers/Feed/components/cards/ContactCard.tsx
+++ src/containers/Feed/components/cards/ContactCard.tsx
@@ -17,7 +17,7 @@ const ContactCard: React.FC = () => {
             href={`https://github.com/${CONFIG.profile.github}`}
             rel="noreferrer"
             target="_blank"
-            className="p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white "
+            className="p-3 hover:bg-gray-100 dark:hover:bg-zinc-800 rounded-2xl cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white "
           >
             <AiOutlineGithub className="text-2xl" />
             <div className="text-sm">github</div>
@@ -28,7 +28,7 @@ const ContactCard: React.FC = () => {
             href={`mailto:${CONFIG.profile.email}`}
             rel="noreferrer"
             target="_blank"
-            className="overflow-hidden p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
+            className="overflow-hidden p-3 hover:bg-gray-100 dark:hover:bg-zinc-800 rounded-2xl cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
           >
             <AiOutlineMail className="text-2xl flex-shrink-0" />
             <div className="text-sm">email</div>
@@ -39,7 +39,7 @@ const ContactCard: React.FC = () => {
             href={`https://www.goodreads.com/${CONFIG.profile.goodreads}`}
             rel="noreferrer"
             target="_blank"
-            className="  overflow-hidden p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
+            className="  overflow-hidden p-3 hover:bg-gray-100 dark:hover:bg-zinc-800 rounded-2xl cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
           >
             <AiOutlineRead className="text-2xl flex-shrink-0" />
             <div className="text-sm">goodreads</div>
